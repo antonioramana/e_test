576668b57b193fe300f1e7a5b1058bd4510e794e
antonioramana <antonioramanandraibe@gmail.com>
2024-03-13 11:11:36 +0300

    feat(add-subjects-with-questions-answers): ajout du suject avec des questions

---
+++ app/Models/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['question, point'];
+
+    public function subjects()
+    {
+        return $this->belongsToMany(Subject::class);
+    }
+}
